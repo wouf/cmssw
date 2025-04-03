1f2b2f7c4f8d872c2aa60cb64c4c176b67950bd0
Andrey <wouf@ilst.ru>
2025-04-03 15:39:34 +0200

    test CELLO cofigs

--- Configuration/Generator/python/Hydjet_Quenched_MinBias_5362GeV_cfi.py
+++ Configuration/Generator/python/Hydjet_Quenched_MinBias_5362GeV_cfi.py
@@ -1,16 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 
-from Configuration.Generator.Pyquen2015Settings_cff import *
+from Configuration.Generator.Pyquen2025Settings_cff import *
+from GeneratorInterface.Core.ExternalGeneratorFilter import ExternalGeneratorFilter
 
-generator = cms.EDFilter("HydjetGeneratorFilter",
-                         collisionParameters5362GeV,
-                         qgpParameters2023,
-                         hydjetParameters2023,
+import sys, os
+# Check if variable "energy" is defined in the main script
+if "energy" in sys.modules["__main__"].__dict__:
+    energy = sys.modules["__main__"].__dict__["energy"]
+else: energy = os.getenv("HJENERGY", "5362")
+
+generator = ExternalGeneratorFilter(cms.EDFilter("HydjetGeneratorFilter",
+                         locals()[f"collisionParameters{energy}GeV"],   #tune CELLO
+                         locals()[f"qgpParameters{energy}GeV"],         #tune CELLO
+                         locals()[f"hydjetParameters{energy}GeV"],      #tune CELLO
                          hydjetMode = cms.string('kHydroQJets'),
                          PythiaParameters = cms.PSet(pyquenPythiaDefaultBlock,
                                                      # Quarkonia and Weak Bosons added back upon dilepton group's request.
                                                      parameterSets = cms.vstring('pythiaUESettings',
-                                                                                 'hydjetPythiaDefault',
+                                                                                 'hydjetPythiaDefault'+energy, #tune CELLO
                                                                                  'myParameters',
                                                                                  'pythiaJets',
                                                                                  'pythiaPromptPhotons',
@@ -23,6 +30,6 @@ generator = cms.EDFilter("HydjetGeneratorFilter",
                                                      ),
                          cFlag = cms.int32(1),
                          bMin = cms.double(0),
-                         bMax = cms.double(30),
+                         bMax = cms.double(22),
                          bFixed = cms.double(0)
-                         )
+                         ))
